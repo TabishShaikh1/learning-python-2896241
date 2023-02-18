6cc93fb848b94106330341318107b8f0facfc2d7
TabishShaikh1 <tabish.s91@gmail.com>
2023-02-18 16:16:53 +0530

    Modules

--- Ch2 - Basics/modules_finished.py
+++ Ch2 - Basics/modules_finished.py
@@ -6,7 +6,7 @@ import math
 
 # the math module contains lots of pre-built functions
 print("The square root of 16 is", math.sqrt(16))
-
+print("The power  of 2 is", math.pow(2,5))
 # in addition to functions, some modules contain useful constants 
 print("Pi is:", math.pi)
 
